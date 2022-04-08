a80f0da930d3065fbe02326a723956fa50ccb4d1
玖二 <102468772+yaomiao95@users.noreply.github.com>
2022-04-09 00:26:07 +0800

    Add files via upload
    not fully built

---
+++ 2017problem2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main() {
+	int a;
+	char ostr;/*create ostr[length]*/
+	char rstr;/*Variate ostr[] into binary form i.e 1011001*/
+	return 0;
+}
