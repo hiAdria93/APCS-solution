e0c039e88cd45e1b17e0e291cb04771cd188f25a
玖二 <102468772+yaomiao95@users.noreply.github.com>
2022-04-10 00:42:14 +0800

    Add files via upload
    not complete

---
+++ 2016problem1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char** argv) {
+	int a,i,p;
+/*a for total amounts of details; i for counts; p stands for pass*/
+	scanf("%d\n",&a);
+	int score[a];
+	for(i==0;i<a;i++){/*input scores in score[]*/
+		scanf("%d",&score[i]);
+	}
+	for(i==0;i<a;i++){
+		if(score[i]>=60){
+			
+		})
+	}
+}
